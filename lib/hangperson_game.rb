708fe9289745cd6cf2160fab45ed2a7b5f19be2e
zachchao <zach.chao@yahoo.com>
2019-09-27 08:09:46 -0700

    problem 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,9 @@
 class HangpersonGame
 
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -10,7 +14,47 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  def guess(letter)
+    if letter == '' or letter == nil or letter =~ /[^A-Za-z]/
+      raise ArgumentError
+    end
+
+    letter.downcase!
+    if (@guesses + @wrong_guesses).include? letter
+      return false
+    end
+    if @word.include? letter
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+  end
+
+  def word_with_guesses()
+    res = "-" * @word.length
+
+    for i in 0..@word.length - 1
+      if guesses.include? word[i]
+        res[i] = word[i]
+      end
+    end
+
+    return res
+  end
+
+  def check_win_or_lose()
+    if @wrong_guesses.length > 6
+      return :lose
+    elsif word_with_guesses.include? '-'
+      return :play
+    else
+      return :win
+    end
+  end 
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
